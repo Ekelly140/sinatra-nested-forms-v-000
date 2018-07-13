23176c925cbf39c126a52352fd9e3775e39e7388
ERIC KELLY <eakelly220@gmail.com>
2018-07-13 22:46:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,7 +12,15 @@ module FormsLab
     end
 
     get '/post' do
-      @pirates =
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ship].each do |details|
+        SShip.new(details)
+      end
+
+      @ships = ship.all
+
+
       erb :"pirates/show"
     end
 
