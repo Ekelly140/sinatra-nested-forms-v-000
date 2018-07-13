ef068fa84dec6b2e0eb48a0a279b95ffc884be91
ERIC KELLY <eakelly220@gmail.com>
2018-07-13 21:35:53 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_reader :name, :age, :weight
+
+PIRATES = []
+
+def initialize(args)
+  @name = args[:name]
+  @age = args[:age]
+  @age = args[:weight]
+  PIRATES << self
+end
+
+def self.all
+  PIRATES
+end
+
 end
